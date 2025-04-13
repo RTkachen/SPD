7969f06a26cbc101d59484202ac689632b0d8af3
rt <rvt2004@outlook.com>
2025-04-13 22:41:48 +0200

    +time measurement for all algorythms

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include "Zadanie.hpp"
 #include "Problem.hpp"
 
+// Funkcja pomocnicza do mierzenia czasu działania
+void measureExecutionTime(const std::string& label, const std::function<void()>& func) {
+    auto start = std::chrono::high_resolution_clock::now();
+    
+    func(); // Wykonanie przekazanej funkcji
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    
+    std::cout << "Czas wykonania (" << label << "): "
+              << std::fixed << std::setprecision(7) << duration.count() << " s" << std::endl;
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         std::cerr << "Użycie: " << argv[0] << " <ścieżka do pliku>" << std::endl;
@@ -12,30 +25,42 @@ int main(int argc, char* argv[]) {
 
     // Tworzymy obiekt Zadanie i wczytujemy dane z pliku
     Zadanie zadanie(filename);
-    zadanie.displayFile();
+    //zadanie.displayFile();
     // Tworzymy obiekt Problem z danymi z Zadania
+    std::cout<<zadanie.getN()<<std::endl;
     Problem problem(zadanie.getInstantion(), zadanie.getN());
     
-    auto start = std::chrono::high_resolution_clock::now();
-    // Przeglad zupelny
-    problem.generatePermutations();
-
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> duration = end - start;
-    std::cout << "Czas wykonania: " << std::fixed << std::setprecision(3) << duration.count() << " s" << std::endl;
-
-    // Wyswietlamy wynik
+    measureExecutionTime("Przegląd zupełny", [&]() {
+        problem.generatePermutations();
+    });
     problem.displayResults();
 
-    //problem.sortByQ();
-    //problem.displayResults();
-    //problem.sortByR();
-    //problem.displayResults();
-    problem.schrage();
+    measureExecutionTime("Sortowanie po q", [&]() {
+        problem.sortByQ();
+    });
     problem.displayResults();
-    problem.schragePmtn();
+
+    measureExecutionTime("Sortowanie po r", [&]() {
+        problem.sortByR();
+    });
     problem.displayResults();
-    problem.customHeuristic();
+
+    measureExecutionTime("Schrage", [&]() {
+        problem.schrage();
+    });
     problem.displayResults();
+
+    measureExecutionTime("Schrage z wywłaszczeniem", [&]() {
+        problem.schragePmtn();
+    });
+    problem.displayResults();
+
+    measureExecutionTime("Własna heurystyka", [&]() {
+        problem.customHeuristic();
+    });
+    problem.displayResults();
+
+    return 0;
+
     return 0;
 }
